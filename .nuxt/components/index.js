4fe529c83eb223d22ff3bd00aea549533f65e9fc
Maxim Kunaev <maksim.kunaev@westarp.io>
2022-06-03 21:14:53 +0200

    v0.0.0: add generated files

---
+++ .nuxt/components/index.js
@@ -0,0 +1,46 @@
+export { default as Card } from '../../components/Card.vue'
+export { default as List } from '../../components/List.vue'
+export { default as Loader } from '../../components/Loader.vue'
+export { default as Bio } from '../../components/Bio/Bio.vue'
+export { default as ExperienceCompany } from '../../components/Experience/Company.vue'
+export { default as Experience } from '../../components/Experience/Experience.vue'
+export { default as Skills } from '../../components/Skills/Skills.vue'
+export { default as TheHeader } from '../../components/TheHeader/TheHeader.vue'
+export { default as Socials } from '../../components/Socials/Socials.vue'
+export { default as ThemeSwitcher } from '../../components/Theme/Switcher.vue'
+export { default as Theme } from '../../components/Theme/Theme.vue'
+export { default as ThemeThemes } from '../../components/Theme/themes.js'
+export { default as ThemeUtilsThemes } from '../../components/Theme/utils/themes.js'
+export { default as ControlsRadio } from '../../components/controls/Radio/Radio.vue'
+export { default as ControlsCheckbox } from '../../components/controls/Checkbox/Checkbox.vue'
+
+// nuxt/nuxt.js#8607
+function wrapFunctional(options) {
+  if (!options || !options.functional) {
+    return options
+  }
+
+  const propKeys = Array.isArray(options.props) ? options.props : Object.keys(options.props || {})
+
+  return {
+    render(h) {
+      const attrs = {}
+      const props = {}
+
+      for (const key in this.$attrs) {
+        if (propKeys.includes(key)) {
+          props[key] = this.$attrs[key]
+        } else {
+          attrs[key] = this.$attrs[key]
+        }
+      }
+
+      return h(options, {
+        on: this.$listeners,
+        attrs,
+        props,
+        scopedSlots: this.$scopedSlots,
+      }, this.$slots.default)
+    }
+  }
+}
---
+++ .nuxt/components/readme.md
@@ -0,0 +1,23 @@
+# Discovered Components
+
+This is an auto-generated list of components discovered by [nuxt/components](https://github.com/nuxt/components).
+
+You can directly use them in pages and other components without the need to import them.
+
+**Tip:** If a component is conditionally rendered with `v-if` and is big, it is better to use `Lazy` or `lazy-` prefix to lazy load.
+
+- `<Card>` | `<card>` (components/Card.vue)
+- `<List>` | `<list>` (components/List.vue)
+- `<Loader>` | `<loader>` (components/Loader.vue)
+- `<Bio>` | `<bio>` (components/Bio/Bio.vue)
+- `<ExperienceCompany>` | `<experience-company>` (components/Experience/Company.vue)
+- `<Experience>` | `<experience>` (components/Experience/Experience.vue)
+- `<Skills>` | `<skills>` (components/Skills/Skills.vue)
+- `<TheHeader>` | `<the-header>` (components/TheHeader/TheHeader.vue)
+- `<Socials>` | `<socials>` (components/Socials/Socials.vue)
+- `<ThemeSwitcher>` | `<theme-switcher>` (components/Theme/Switcher.vue)
+- `<Theme>` | `<theme>` (components/Theme/Theme.vue)
+- `<ThemeThemes>` | `<theme-themes>` (components/Theme/themes.js)
+- `<ThemeUtilsThemes>` | `<theme-utils-themes>` (components/Theme/utils/themes.js)
+- `<ControlsRadio>` | `<controls-radio>` (components/controls/Radio/Radio.vue)
+- `<ControlsCheckbox>` | `<controls-checkbox>` (components/controls/Checkbox/Checkbox.vue)
